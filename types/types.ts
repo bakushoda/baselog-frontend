9a6cd4d994ada39e41cb6a3125f5ddbe5d3a5899
bakushoda <66668783+bakushoda@users.noreply.github.com>
2024-11-10 22:04:23 +0900

    refactor: addgamepage

--- types/types.ts
+++ types/types.ts
@@ -32,3 +32,16 @@ export interface Game {
   away_inning_8: number;
   away_inning_9: number;
 }
+
+export interface GameData {
+    date: string;
+    home_team: string;
+    away_team: string;
+    home_total_score: number;
+    away_total_score: number;
+    home_total_hits: number;
+    away_total_hits: number;
+    home_total_errors: number;
+    away_total_errors: number;
+    [key: `home_inning_${number}` | `away_inning_${number}`]: number;
+  }
